25003d44b59d5d4223ce9bbd21f45962c53e5c8a
Jacinto Martinez <jacintomartinez0909@gmail.com>
2023-03-13 23:12:31 +0100

    Deleted imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Search from './components/Search'
 import Meal from './components/Meal'
 import { Navbar, Text } from '@nextui-org/react'
-import './App.css'
 import { Link, Route, Routes, BrowserRouter } from 'react-router-dom'
 
 function App () {
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { NextUIProvider, createTheme } from '@nextui-org/react'
-import './index.css'
 
 const darkTheme = createTheme({
   type: 'dark'
